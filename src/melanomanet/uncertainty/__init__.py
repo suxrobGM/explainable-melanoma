bcdf8d586c77e2a5bcbf375118aed3050adae78e
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2025-12-09 21:03:22 -0500

    removed ununsed temprature scaling

--- src/melanomanet/uncertainty/__init__.py
+++ src/melanomanet/uncertainty/__init__.py
@@ -8,12 +8,10 @@ Uncertainty Estimation Module.
 
 from .mc_dropout import MCDropoutEstimator
 from .models import UncertaintyResult
-from .temprature_scaling import TemperatureScaling
 from .utils import compute_calibration_metrics, get_uncertainty_interpretation
 
 __all__ = [
     "MCDropoutEstimator",
-    "TemperatureScaling",
     "UncertaintyResult",
     "compute_calibration_metrics",
     "get_uncertainty_interpretation",
